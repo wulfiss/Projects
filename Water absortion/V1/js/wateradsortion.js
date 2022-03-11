431e653e43e2d9259b91ee85ce6346468040166d
wulfiss <96155980+wulfiss@users.noreply.github.com>
2022-03-11 02:45:53 -0300

    change number of chickens

--- Water absortion/V1/js/wateradsortion.js
+++ Water absortion/V1/js/wateradsortion.js
@@ -41,7 +41,7 @@ function averangeAbsortionArray(){
 }
 
 function getRandomArbitrarySeal(max, min){
-    let p = m + 2;
+    let p = m + 5;
     for (let n = 0; n < p; n++){
         numberSeal[n] = parseFloat((Math.random() * (max - min) + min).toFixed());
     }
@@ -50,7 +50,7 @@ function getRandomArbitrarySeal(max, min){
 function getRandomArbitrarychicken(avChicken){
     let max = avChicken + 0.350;
     let min = avChicken - 0.350;
-    let p = m + 2;
+    let p = m + 5;
     for (let n = 0; n < p; n++){
         numberChicken[n] = parseFloat((Math.random() * (max - min) + min).toFixed(3));
     }
@@ -80,7 +80,7 @@ let m = 20;
 let $buttonGenerateTable = document.getElementById("show-table");
 
 $buttonGenerateTable.onclick = function(){
-    createTable(22, 6);
+    createTable(25, 6);
 
     let chickenAv = parseFloat(document.getElementById("average-chicken-weight").value);
     let sealMax = parseFloat(document.getElementById("seal-max").value);
@@ -89,6 +89,9 @@ $buttonGenerateTable.onclick = function(){
     let waterMin = parseFloat(document.getElementById("water-min").value);
     let sealLost1 = parseFloat(document.getElementById("seal-lost1").value);
     let sealLost2 = parseFloat(document.getElementById("seal-lost2").value);
+    let sealLost3 = parseFloat(document.getElementById("seal-lost3").value);
+    let sealLost4 = parseFloat(document.getElementById("seal-lost4").value);
+    let sealLost5 = parseFloat(document.getElementById("seal-lost5").value);
 
     function comprobation(){
         if(averangeAbsortionArray() <= waterMax && averangeAbsortionArray() >= waterMin){
@@ -124,19 +127,19 @@ $buttonGenerateTable.onclick = function(){
     let x = document.getElementById('testBody').getElementsByTagName('td')
 
     let b = 1;
-    for(let k = 0; k <132; k+=6){
+    for(let k = 0; k < 150; k+=6){
         x[k].textContent = b;
         b++;
     }
 
     b = 0;
-    for (let k = 1; k < 132; k+=6){
+    for (let k = 1; k < 150; k+=6){
         x[k].textContent = numberSeal[b];
         b++;
     }
 
     b = 0;
-    for(let k = 2; k < 132; k+=6){
+    for(let k = 2; k < 150; k+=6){
         x[k].textContent = numberChicken[b].toFixed(3);
         b++;
     }
@@ -185,6 +188,48 @@ $buttonGenerateTable.onclick = function(){
         x[sealLost2C+3].textContent = "----";
         x[sealLost2C+4].textContent = "----";
         x[sealLost2C+5].textContent = "----";
+    }
+
+    if(sealLost3 != 23){
+        x[134].textContent = numberChicken[sealLost3-1].toFixed(3);
+        x[135].textContent = finalChickenW[sealLost3-1].toFixed(3);
+        x[136].textContent = subtrationChickenArray[sealLost3-1].toFixed(3);
+        x[137].textContent = sumA[sealLost3-1].toFixed(2);
+
+        let sealLost3C = ((sealLost3 - 1) * 6);
+        
+        x[sealLost3C+2].textContent = numberChicken[22].toFixed(3);
+        x[sealLost3C+3].textContent = "----";
+        x[sealLost3C+4].textContent = "----";
+        x[sealLost3C+5].textContent = "----";
+    }
+
+    if(sealLost2 != 24){
+        x[140].textContent = numberChicken[sealLost4-1].toFixed(3);
+        x[141].textContent = finalChickenW[sealLost4-1].toFixed(3);
+        x[142].textContent = subtrationChickenArray[sealLost4-1].toFixed(3);
+        x[143].textContent = sumA[sealLost4-1].toFixed(2);
+
+        let sealLost4C = ((sealLost4 - 1) * 6);
+        
+        x[sealLost4C+2].textContent = numberChicken[23].toFixed(3);
+        x[sealLost4C+3].textContent = "----";
+        x[sealLost4C+4].textContent = "----";
+        x[sealLost5C+5].textContent = "----";
+    }
+
+    if(sealLost2 != 25){
+        x[146].textContent = numberChicken[sealLost5-1].toFixed(3);
+        x[147].textContent = finalChickenW[sealLost5-1].toFixed(3);
+        x[148].textContent = subtrationChickenArray[sealLost5-1].toFixed(3);
+        x[149].textContent = sumA[sealLost5-1].toFixed(2);
+
+        let sealLost5C = ((sealLost5 - 1) * 6);
+        
+        x[sealLost5C+2].textContent = numberChicken[24].toFixed(3);
+        x[sealLost5C+3].textContent = "----";
+        x[sealLost5C+4].textContent = "----";
+        x[sealLost5C+5].textContent = "----";
     }
     
     tr = testBody.insertRow();
